a14d694dae1a3da5d112ddf7bd0b23c716de4b28
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-05-22 17:31:29 +0300

    encrypt password

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -62,3 +62,18 @@ password = "MyAmazingPassw0rd"
 encrypted_password = hash_password(password)
 print(encrypted_password)
 print(is_valid(encrypted_password, password))
+
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+get_db = __import__('filtered_logger').get_db
+
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()
